69b0a5cbec166e86b36c4ce2b1121d685aab5b15
Draganhristovic <gagi.hristovic@gmail.com>
2018-08-29 20:03:02 +0200

    Prva verziija CV-a

---
+++ scripts/script.js
@@ -0,0 +1,57 @@
+var animirano = false;
+
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems, {
+        onOpenEnd: function () {
+            document.querySelector("li.active").scrollIntoView({ behavior: "smooth" });
+        }
+    });
+
+    var instances = M.Collapsible.init(elems, {
+        onOpenEnd: function (el) {
+            var sb = el.querySelectorAll(".skillsBox>.skill");
+
+            if (sb.length > 0) {
+                var i;
+                for (i = 0; i < sb.length; i++) {
+                    sb[i].children[0].classList.add("skill-anim");
+                }
+            }
+        },
+
+        onCloseEnd: function (el) {
+            var sb = el.querySelectorAll(".skillsBox>.skill");
+
+            if (sb.length > 0) {
+                var i;
+                for (i = 0; i < sb.length; i++) {
+                    sb[i].children[0].classList.remove("skill-anim");
+                }
+            }
+        }
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("person").addEventListener("click", animirajIliVrati);
+});
+
+function animirajIliVrati() {
+    if (animirano) {
+        document.querySelector("#anim").classList.remove("animacija");
+    }
+    else {
+        document.querySelector("#anim").classList.add("animacija");
+    }
+
+    animirano = !animirano;
+}
+
+function printCV() {
+    document.getElementById("cvPDF").contentWindow.postMessage("print", "*");
+}
+
+function downloadCV() {
+    alert("CV se preuzima");
+}
